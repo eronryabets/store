3880fbbfa5b5dd083739d1a8733dbf125a7f8be4
eronryabets <eronryabets@gmail.com>
2024-03-15 16:42:41 +0200

    Transfer to postgresql.

--- products/admin.py
+++ products/admin.py
@@ -8,8 +8,8 @@ admin.site.register(ProductCategory)
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'quantity', 'category')
-    fields = ('image', 'description', ('price', 'quantity'), 'name', 'category')
-    readonly_fields = ('description',)
+    fields = ('image', 'name', 'category', 'description', ('price', 'quantity'), )
+    # readonly_fields = ('description',)
     search_fields = ('name',)
     ordering = ('name',)
 
