362ee1bccab31b780ad999480fa200eee513b15f
eronryabets <eronryabets@gmail.com>
2024-03-25 16:41:00 +0200

    Added ordering page.

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 
-from orders.views import OrderCreateView, SuccessTemplateView, CanceledTemplateView
+from orders.views import OrderCreateView, SuccessTemplateView, CanceledTemplateView, OrderListView
 
 app_name = 'orders'
 
@@ -9,5 +9,6 @@ urlpatterns = [
     path('order-create/', OrderCreateView.as_view(), name='order_create'),
     path('order-success/', SuccessTemplateView.as_view(), name='order_success'),
     path('order-canceled/', CanceledTemplateView.as_view(), name='order_canceled'),
+    path('', OrderListView.as_view(), name='orders_list'),
 
 ]
--- orders/views.py
+++ orders/views.py
@@ -4,7 +4,7 @@ from http import HTTPStatus
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse, reverse_lazy
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, TemplateView, ListView
 
 from common.views import TitleMixin
 from orders.forms import OrderForm
@@ -22,6 +22,17 @@ class SuccessTemplateView(TitleMixin, TemplateView):
 
 class CanceledTemplateView(TemplateView):
     template_name = 'orders/canceled.html'
+
+
+class OrderListView(TitleMixin, ListView):
+    template_name = 'orders/orders.html'
+    title = 'Store - Orders'
+    queryset = Order.objects.all()
+    ordering = ['-created']
+
+    def get_queryset(self):
+        queryset = super(OrderListView, self).get_queryset()
+        return queryset.filter(initiator=self.request.user)
 
 
 class OrderCreateView(TitleMixin, CreateView):
